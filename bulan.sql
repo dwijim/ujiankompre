3bbcefa43811d401e80f55c7d4937673fc5b9363
dwi sakethi <44912078+dwijim@users.noreply.github.com>
2022-04-02 14:28:44 +0700

    Add files via upload

---
+++ bulan.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 02, 2022 at 02:27 PM
+-- Server version: 10.3.34-MariaDB-0ubuntu0.20.04.1
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pusdatin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bulan`
+--
+
+CREATE TABLE `bulan` (
+  `kode` char(2) NOT NULL,
+  `keterangan` varchar(27) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bulan`
+--
+
+INSERT INTO `bulan` (`kode`, `keterangan`) VALUES
+('0', 'Semua bulan'),
+('1', 'Januari'),
+('10', 'Oktober'),
+('11', 'November'),
+('12', 'Desember'),
+('2', 'Februari'),
+('3', 'Maret'),
+('4', 'April'),
+('5', 'Mei'),
+('6', 'Juni'),
+('7', 'Juli'),
+('8', 'Agustus'),
+('9', 'September');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bulan`
+--
+ALTER TABLE `bulan`
+  ADD PRIMARY KEY (`kode`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
