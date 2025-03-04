bb7fc9216454b2728dbaacbdce48f610089f46da
dwi sakethi <44912078+dwijim@users.noreply.github.com>
2025-03-04 12:58:19 +0700

    Update bulan.sql

--- bulan.sql
+++ bulan.sql
@@ -38,18 +38,18 @@ CREATE TABLE `bulan` (
 --
 
 INSERT INTO `bulan` (`kode`, `keterangan`) VALUES
-('1', 'Januari'),
+('1 ', 'Januari'),
 ('10', 'Oktober'),
 ('11', 'November'),
 ('12', 'Desember'),
-('2', 'Februari'),
-('3', 'Maret'),
-('4', 'April'),
-('5', 'Mei'),
-('6', 'Juni'),
-('7', 'Juli'),
-('8', 'Agustus'),
-('9', 'September');
+('2 ', 'Februari'),
+('3 ', 'Maret'),
+('4 ', 'April'),
+('5 ', 'Mei'),
+('6 ', 'Juni'),
+('7 ', 'Juli'),
+('8 ', 'Agustus'),
+('9 ', 'September');
 
 --
 -- Indexes for dumped tables
